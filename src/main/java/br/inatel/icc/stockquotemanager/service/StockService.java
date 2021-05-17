f581621681c4f45ec98926eb539a87b82181fa5e
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-17 17:28:08 -0300

    put stocks in cache

--- src/main/java/br/inatel/icc/stockquotemanager/service/StockService.java
+++ src/main/java/br/inatel/icc/stockquotemanager/service/StockService.java
@@ -3,10 +3,13 @@ package br.inatel.icc.stockquotemanager.service;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import br.inatel.icc.stockquotemanager.dto.StockDto;
 
+@Service
 public class StockService {
 
 	private String defaultUrl = "http://localhost:8080";
@@ -16,11 +19,13 @@ public class StockService {
 		restTemplate = new RestTemplate();
 	}
 
+	@Cacheable(value = "stocks")
 	public List<StockDto> getAll() {
 		StockDto[] stocks = restTemplate.getForObject("http://localhost:8080/stock", StockDto[].class);
 		return Arrays.asList(stocks);
 	}
 	
+	@Cacheable(value = "stock")
 	public StockDto getById(String id) {
 		StockDto stock = restTemplate.getForObject(defaultUrl + "/stock/" + id, StockDto.class);
 		return stock;
