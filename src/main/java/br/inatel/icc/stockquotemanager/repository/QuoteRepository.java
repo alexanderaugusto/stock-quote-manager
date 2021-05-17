c547cda328d51b052c60602345e8129d1113cf49
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-17 14:38:03 -0300

    create method to list stock quotes by stock id

--- src/main/java/br/inatel/icc/stockquotemanager/repository/QuoteRepository.java
+++ src/main/java/br/inatel/icc/stockquotemanager/repository/QuoteRepository.java
@@ -1,9 +1,12 @@
 package br.inatel.icc.stockquotemanager.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.inatel.icc.stockquotemanager.model.Quote;
 
 public interface QuoteRepository extends JpaRepository<Quote, Long>{
 
+	List<Quote> findByStockId(String stockId);
 }
