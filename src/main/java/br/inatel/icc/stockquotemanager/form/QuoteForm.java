0ddbdb0f6342d655bcebecbb5956de6f88569764
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-18 19:50:53 -0300

    change quote form to receive data from client

--- src/main/java/br/inatel/icc/stockquotemanager/form/QuoteForm.java
+++ src/main/java/br/inatel/icc/stockquotemanager/form/QuoteForm.java
@@ -2,33 +2,35 @@ package br.inatel.icc.stockquotemanager.form;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import br.inatel.icc.stockquotemanager.model.Quote;
 
 public class QuoteForm {
 
-	private String date;
-	private String value;
-	private String stockId;
+	private String id;
+	private Map<String, String> quotes;
+
+	public String getId() {
+		return id;
+	}
+
+	public Map<String, String> getQuotes() {
+		return quotes;
+	}
 	
-	public String getDate() {
-		return date;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public String getStockId() {
-		return stockId;
-	}
-
-	public Quote toQuote() {
-		LocalDate date = LocalDate.parse(this.date);
-		BigDecimal value = new BigDecimal(this.value);
+	public List<Quote> convertQuotesMapToList(){
+		List<Quote> quotesList = new ArrayList<>();
 		
-		Quote quote = new Quote(date, value, stockId);
+		for (Map.Entry<String, String> quote : this.quotes.entrySet()) {
+			LocalDate date = LocalDate.parse(quote.getKey());
+			BigDecimal value = new BigDecimal(quote.getValue());
+			
+		    quotesList.add(new Quote(date, value, this.id));
+		}
 		
-		return quote;
+		return quotesList;
 	}
 }
