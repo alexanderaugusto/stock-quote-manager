581250f5b5a328bf88e1e397af5978b443b2b4fb
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-19 09:54:33 -0300

    remove unused loggin static attribute

--- src/main/java/br/inatel/icc/stockquotemanager/StockQuoteManagerApplication.java
+++ src/main/java/br/inatel/icc/stockquotemanager/StockQuoteManagerApplication.java
@@ -1,7 +1,5 @@
 package br.inatel.icc.stockquotemanager;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,8 +8,6 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 public class StockQuoteManagerApplication {
 
-	public static final Logger logger = LogManager.getLogger(StockQuoteManagerApplication.class);
-	
 	public static void main(String[] args) {
 		SpringApplication.run(StockQuoteManagerApplication.class, args);
 	}
