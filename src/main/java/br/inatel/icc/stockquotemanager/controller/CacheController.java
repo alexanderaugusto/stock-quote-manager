36a20d1c13e2e15d769da55f4fb0a438ccfb524d
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-20 14:25:45 -0300

    clean only stocks cache when call delete method

--- src/main/java/br/inatel/icc/stockquotemanager/controller/CacheController.java
+++ src/main/java/br/inatel/icc/stockquotemanager/controller/CacheController.java
@@ -3,7 +3,6 @@ package br.inatel.icc.stockquotemanager.controller;
 import javax.transaction.Transactional;
 
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,8 @@ public class CacheController {
 	
 	@DeleteMapping
 	@Transactional
-	@Caching(evict = { @CacheEvict(value = "stocks", allEntries = true), @CacheEvict(value = "stock", allEntries = true) })
+//	@Caching(evict = { @CacheEvict(value = "stocks", allEntries = true), @CacheEvict(value = "stock", allEntries = true) })
+	@CacheEvict(value = "stocks", allEntries = true)
 	public ResponseEntity<?> cleanCache() {
 		log.info("cleaning cache");
 		return ResponseEntity.status(204).build();
